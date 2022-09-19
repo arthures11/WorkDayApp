b6f9003721144ff30feb9cd358117cc4309a9d82
456 <58474213+arthures11@users.noreply.github.com>
2022-09-19 06:54:33 +0200

    add services

--- src/main/java/bryja/com/WorkDayApp/Controllers/WorkDayController.java
+++ src/main/java/bryja/com/WorkDayApp/Controllers/WorkDayController.java
@@ -15,112 +15,56 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.BeanUtils;
+import bryja.com.WorkDayApp.Services.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class WorkDayController {
-    private final WorkDayRepository repository;
-    private final TimeEntryRepository entries_repository;
     @Autowired
-    WorkDayController(WorkDayRepository repository, TimeEntryRepository entries_repository) {
-        this.repository = repository;
-        this.entries_repository = entries_repository;
-    }
+    private WorkDayService workDayService;
 
 
     @GetMapping("/workdays")
-    List<WorkDay> showAllWorkdays() {
-        return repository.findAll();
+    public List<WorkDay> showAllWorkdays() {
+        return workDayService.showAllWorkdays();
     }
     @PostMapping("/workdays")
     WorkDay newWorkDay(@RequestBody WorkDay newWorkDay) {
-        GregorianDateMatcher datematch = new GregorianDateMatcher();
-        if(datematch.matches(newWorkDay.date)){
-            return repository.save(newWorkDay);
-        }
-        else {
-            throw new WorkDayNotFoundException(newWorkDay.date);
-        }
+        return workDayService.newWorkDay(newWorkDay);
     }
 
     @PostMapping("/workdays/{id}/entries")
     void addEntryToWorkday(@RequestBody TimeEntry entry, @PathVariable Long id) {
-            WorkDay WorkDay = repository.findById(id)
-                    .orElseThrow(() -> new WorkDayNotFoundException(id));
-           // WorkDay.getTimeEntry().add(new TimeEntry(entry.description, entry.time_spent));
-          //  WorkDay.date = "abc";
-          //  repository.save(WorkDay);
-
-        TimeEntry TimeEntry = repository.findById(id).map(workday -> {
-            entry.setWorkday(workday);
-            return entries_repository.save(entry);
-        }).orElseThrow(() -> new WorkDayNotFoundException(id));
-       // return new ResponseEntity<>(comment, HttpStatus.CREATED);
-
+         workDayService.addEntryToWorkday(entry,id);
     }
 
     @PutMapping("/workdays/{id}/entries/{id2}")
     void updateTimeEntry(@RequestBody TimeEntry entry, @PathVariable Long id,@PathVariable Long id2) {
-
-        repository.findById(id).orElseThrow(() -> new WorkDayNotFoundException(id));
-        entries_repository.findById(id2).map(timeEntry -> {
-            timeEntry.time_spent=entry.time_spent;
-            timeEntry.description=entry.description;
-            return entries_repository.save(timeEntry);}
-                )
-                .orElseThrow(() -> new WorkDayNotFoundException(id2));
+        workDayService.updateTimeEntry(entry,id,id2);
     }
 
 
     @GetMapping("/workdays/{id}")
-    EntityModel<WorkDay> showSpecificWorkday(@PathVariable Long id) {
-
-        WorkDay WorkDay = repository.findById(id) //
-                .orElseThrow(() -> new WorkDayNotFoundException(id));
-
-        return EntityModel.of(WorkDay, //
-                linkTo(methodOn(WorkDayController.class).showSpecificWorkday(id)).withSelfRel(),
-                linkTo(methodOn(WorkDayController.class).showAllWorkdays()).withRel("workdays"));
+    public EntityModel<WorkDay> showSpecificWorkday(@PathVariable Long id) {
+        return workDayService.showSpecificWorkday(id);
     }
     @GetMapping("/workdays/{id}/entries")
     List<TimeEntry> showSpecificEntries(@PathVariable Long id) {
-        WorkDay WorkDay = repository.findById(id) //
-                .orElseThrow(() -> new WorkDayNotFoundException(id));
-        return WorkDay.getTimeEntry();
-
+        return workDayService.showSpecificEntries(id);
         }
 
 
 
     @PutMapping("/workdays/{id}")
     WorkDay updateWorkDay(@RequestBody WorkDay newWorkDay, @PathVariable Long id) {
-        GregorianDateMatcher datematch = new GregorianDateMatcher();
-        if(datematch.matches(newWorkDay.date)){
-            return repository.findById(id)
-                    .map(employee -> {
-                        employee.setDate(newWorkDay.getDate());
-                        return repository.save(employee);
-                    })
-                    .orElseGet(() -> {
-                        newWorkDay.setId(id);
-                        return repository.save(newWorkDay);
-                    });
-        }
-        else{
-            throw new WorkDayNotFoundException(newWorkDay.date);
-        }
+        return workDayService.updateWorkDay(newWorkDay,id);
 
     }
 
     @DeleteMapping("/workdays/{id}")
     void deleteWorkDay(@PathVariable("id") Long id) {
-        WorkDay WorkDay = repository.findById(id) //
-                .orElseThrow(() -> new WorkDayNotFoundException(id));
-        if(!WorkDay.getTimeEntry().isEmpty()){
-          //  WorkDay.getTimeEntry().clear();
-        }
-        repository.deleteById(id);
+         workDayService.deleteWorkDay(id);
     }
 }
---
+++ src/main/java/bryja/com/WorkDayApp/Services/WorkDayService.java
@@ -0,0 +1,115 @@
+package bryja.com.WorkDayApp.Services;
+import java.sql.Time;
+import java.util.*;
+import bryja.com.WorkDayApp.Classes.TimeEntry;
+import bryja.com.WorkDayApp.Classes.WorkDay;
+import bryja.com.WorkDayApp.Controllers.WorkDayController;
+import bryja.com.WorkDayApp.Exceptions.EntryNotFoundException;
+import bryja.com.WorkDayApp.Exceptions.WorkDayNotFoundException;
+import bryja.com.WorkDayApp.Repository.TimeEntryRepository;
+import bryja.com.WorkDayApp.Repository.WorkDayRepository;
+import bryja.com.WorkDayApp.Utility.GregorianDateMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.BeanUtils;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
+@Service
+public class WorkDayService {
+
+    private final WorkDayRepository repository;
+    private final TimeEntryRepository entries_repository;
+
+    @Autowired
+    WorkDayService(WorkDayRepository repository, TimeEntryRepository entries_repository) {
+        this.repository = repository;
+        this.entries_repository = entries_repository;
+    }
+
+    public List<WorkDay> showAllWorkdays() {
+        return repository.findAll();
+    }
+
+    public WorkDay newWorkDay(@RequestBody WorkDay newWorkDay) {
+        GregorianDateMatcher datematch = new GregorianDateMatcher();
+        if(datematch.matches(newWorkDay.date)){
+            return repository.save(newWorkDay);
+        }
+        else {
+            throw new WorkDayNotFoundException(newWorkDay.date);
+        }
+    }
+    public void addEntryToWorkday(@RequestBody TimeEntry entry, @PathVariable Long id) {
+        WorkDay WorkDay = repository.findById(id)
+                .orElseThrow(() -> new WorkDayNotFoundException(id));
+        // WorkDay.getTimeEntry().add(new TimeEntry(entry.description, entry.time_spent));
+        //  WorkDay.date = "abc";
+        //  repository.save(WorkDay);
+
+        TimeEntry TimeEntry = repository.findById(id).map(workday -> {
+            entry.setWorkday(workday);
+            return entries_repository.save(entry);
+        }).orElseThrow(() -> new WorkDayNotFoundException(id));
+        // return new ResponseEntity<>(comment, HttpStatus.CREATED);
+
+    }
+
+    public void updateTimeEntry(@RequestBody TimeEntry entry, @PathVariable Long id,@PathVariable Long id2) {
+
+        repository.findById(id).orElseThrow(() -> new WorkDayNotFoundException(id));
+        entries_repository.findById(id2).map(timeEntry -> {
+                    timeEntry.time_spent=entry.time_spent;
+                    timeEntry.description=entry.description;
+                    return entries_repository.save(timeEntry);}
+                )
+                .orElseThrow(() -> new WorkDayNotFoundException(id2));
+    }
+    public EntityModel<WorkDay> showSpecificWorkday(@PathVariable Long id) {
+
+        WorkDay WorkDay = repository.findById(id) //
+                .orElseThrow(() -> new WorkDayNotFoundException(id));
+
+        return EntityModel.of(WorkDay, //
+                linkTo(methodOn(WorkDayController.class).showSpecificWorkday(id)).withSelfRel(),
+                linkTo(methodOn(WorkDayController.class).showAllWorkdays()).withRel("workdays"));
+    }
+
+    public List<TimeEntry> showSpecificEntries(@PathVariable Long id) {
+        WorkDay WorkDay = repository.findById(id) //
+                .orElseThrow(() -> new WorkDayNotFoundException(id));
+        return WorkDay.getTimeEntry();
+
+    }
+    public WorkDay updateWorkDay(@RequestBody WorkDay newWorkDay, @PathVariable Long id) {
+        GregorianDateMatcher datematch = new GregorianDateMatcher();
+        if(datematch.matches(newWorkDay.date)){
+            return repository.findById(id)
+                    .map(employee -> {
+                        employee.setDate(newWorkDay.getDate());
+                        return repository.save(employee);
+                    })
+                    .orElseGet(() -> {
+                        newWorkDay.setId(id);
+                        return repository.save(newWorkDay);
+                    });
+        }
+        else{
+            throw new WorkDayNotFoundException(newWorkDay.date);
+        }
+
+    }
+    public void deleteWorkDay(@PathVariable("id") Long id) {
+        WorkDay WorkDay = repository.findById(id) //
+                .orElseThrow(() -> new WorkDayNotFoundException(id));
+        if(!WorkDay.getTimeEntry().isEmpty()){
+            //  WorkDay.getTimeEntry().clear();
+        }
+        repository.deleteById(id);
+    }
+}
